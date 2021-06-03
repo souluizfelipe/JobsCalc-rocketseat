cf8a0eb781ed81d000ece4c061b523adce396f9a
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-06-03 15:36:22 -0300

    create profile object to use on profile.ejs as variables

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+    name: 'Luiz Felipe',
+    avatar: 'https://unavatar.vercel.app/github/souluizfelipe',
+    "monthly-budget": 5000.00,
+    "hours-per-day": 8,
+    "days-per-week": 5,
+    "vacation-per-year": 5,
+}
+
 routes.get('/', (req, res) => res.render(views + "index"));
 routes.get('/job', (req, res) => res.render(views + "job"));
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
-routes.get('/profile', (req, res) => res.render(views + "profile"));
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile}));
 
 module.exports = routes;
