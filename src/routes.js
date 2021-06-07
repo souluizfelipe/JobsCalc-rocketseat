e05bc0ca1c6c59d312192230323606f67b5d478c
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-06-07 11:49:18 -0300

    create jobs array and returnd jobs content to index

--- src/routes.js
+++ src/routes.js
@@ -12,10 +12,41 @@ const profile = {
     "vacation-per-year": 5,
 }
 
-routes.get('/', (req, res) => res.render(views + "index", { profile }));
+const jobs = [
+    {
+        id: 1,
+        name: 'Pizzaria Gulozo',
+        'daily-hours': 8,
+        'total-hours' : 28,
+        'created-at': Date.now(), 
+    },
+    {
+        id: 2,
+        name: 'OneTwo Project',
+        'daily-hours': 8,
+        'total-hours' : 190,
+        'created-at': Date.now(), 
+    },
+];
+
+routes.get('/', (req, res) => res.render(views + "index", { jobs, profile }));
 routes.get('/job', (req, res) => res.render(views + "job"));
 routes.post('/job', (req, res) => {
-    console.log(req.body);
+
+    const lastId = jobs[jobs.length - 1]?.id || 0;
+
+    //{ name: 'Nubank', 'daily-hours': '7', 'total-hours': '98' }
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        'daily-hours': req.body['daily-hours'],
+        'total-hours' : req.body['total-hours'],
+        'created-at': Date.now(), 
+    });
+
+    console.log(jobs);
+
+    return res.redirect('/');
 });
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
