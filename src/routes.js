cd37ba69f8d00bbd4ba62b155f31e8ce6e834c83
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-06-08 07:59:41 -0300

    create remaningDays Function on routes to calculate the time left to delivery and add ejs to index

--- src/routes.js
+++ src/routes.js
@@ -10,27 +10,61 @@ const profile = {
     "hours-per-day": 8,
     "days-per-week": 5,
     "vacation-per-year": 5,
+    "value-hour": 75,
 }
 
 const jobs = [
     {
         id: 1,
         name: 'Pizzaria Gulozo',
-        'daily-hours': 8,
-        'total-hours' : 28,
+        'daily-hours': 2,
+        'total-hours' : 1,
         'created-at': Date.now(), 
     },
     {
         id: 2,
         name: 'OneTwo Project',
         'daily-hours': 8,
-        'total-hours' : 190,
+        'total-hours' : 100,
         'created-at': Date.now(), 
     },
 ];
 
-routes.get('/', (req, res) => res.render(views + "index", { jobs, profile }));
+function remaningDays(job) {
+    const remaningDays = (job['total-hours'] / job['daily-hours']).toFixed();
+    
+    const createdDate = new Date(job['created-at']);
+    const dueDay = createdDate.getDate() + Number(remaningDays);
+    const dueDateInMs = createdDate.setDate(dueDay);
+
+    const timeDiffInMs = dueDateInMs - Date.now();
+
+    const dayInMs = 1000 * 60 * 60 * 24 
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+
+    return dayDiff;
+};
+
+routes.get('/', (req, res) => {
+
+    const updateJobs = jobs.map((job) => {
+        const remaning = remaningDays(job);
+        const status = remaning <= 0 ? 'done' : 'progress';
+        
+        return {
+            ...job,
+            remaning,
+            status,
+            budget: profile['value-hour'] * job['total-hours'],
+        }
+    });
+
+    res.render(views + "index", { jobs: updateJobs, profile });
+
+});
+
 routes.get('/job', (req, res) => res.render(views + "job"));
+
 routes.post('/job', (req, res) => {
 
     const lastId = jobs[jobs.length - 1]?.id || 0;
