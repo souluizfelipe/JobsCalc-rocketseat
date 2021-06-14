dd74ecae27dd4b5752eb9dc2f5dd0fb5156b4483
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-06-14 10:19:39 -0300

    create freeHours calculation and add to index.ejs

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -13,20 +13,26 @@ module.exports = {
                 done: 0,
         };
 
+        let jobTotalHours = 0
+
         const updateJobs = jobs.map((job) => {
             const remaning = JobUtils.remaningDays(job);
             const status = remaning <= 0 ? 'done' : 'progress';
 
             statusCount[status] += 1;
 
+            jobTotalHours = status === 'progress' ? jobTotalHours += Number(job['daily-hours']) : jobTotalHours;
+
             return {
                 ...job,
                 remaning,
                 status,
                 budget: JobUtils.calculateBudget(job, profile['value-hour']),
-            }
+            };
         });
+
+        const freeHours = profile["hours-per-day"] - jobTotalHours;
         
-        res.render( "index", { jobs: updateJobs, profile: profile, statusCount: statusCount });
+        res.render( "index", { jobs: updateJobs, profile: profile, statusCount: statusCount, freeHours: freeHours, });
     },
 };
